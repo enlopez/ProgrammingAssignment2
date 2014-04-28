8807bb10d39c8407e99902ca96e83810ac900cce
enlopez <enlopez@espe.edu.ec>
2014-04-27 20:05:47 -0500

    Revert "Update cachematrix.R"
    This reverts commit 589f871f2da325bc11f5f053de4c75f55c4685d8.

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,15 @@
-# # Esta función crea un objeto especial "matriz" que se puede almacenar en caché su inversa
+## Put comments here that give an overall description of what your
+## functions do
 
+## Write a short comment describing this function
 
-# # Esta parte de la función de establecer y obtener el valor de la matriz y 
-# # Establecer y obtener el valor de la inversa de la matriz
-
-
-
-
-makeCacheMatrix <-  función ( x = matriz ())  {
-        inv <- NULL
-        set <- función ( y )  {
-               x << - Y
-               inv << - NULL
-        }
-        conseguir <- función () x
-        setinverse <-  función ( inversa ) inv << - inversa
-        getinverse <-  función () inv
-        lista ( set = set , get = get , setinverse = setinverse , getinverse = getinverse )
+makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-# # Calcula la inversa ya se ha calculado (y la matriz no ha chaged),
-# # Entonces el cachesolve debe retrive el inverso de la caché
 
-cacheSolve <-  función ( x ,  ... )  {
-        inv <- x $ getinverse ()
-        si  ( ! is.null ( i ))  {
-                menssage ( "conseguir los datos en caché" )
-                retorno ( inv )
-        }
-        datos <- x $ get ()
-        inv <- resolver ( datos ,  ... )
-        x $ setinverse ( inv )
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
